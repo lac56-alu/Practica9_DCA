fc45e32693a126405812479cb9e67c4238ac8c08
lac56-alu <lac56@gcloud.ua.es>
2021-12-28 11:49:28 +0100

    Introducir variables

---
+++ src/calculadora.cc
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+
+int main() {
+    float x, y, resultado;
+    char op;
+    int valido = 1;
+
+    cout << "Ingrese operacion: ";
+    cin >> op;
+    cout <<"Ingrese x: ";
+    cin >> x;
+    cout << "Ingrese y: ";
+    cin >> y;
+}
